655c1cd5d284bce20dc8a66c8f7059b82426f847
jayzernguyen <duynq@qsoftvietnam.com>
2022-07-19 20:15:55 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  window.location.href = 'https://gearhead1752.000webhostapp.com/';
   return (
     <div className="App">
       <header className="App-header">
