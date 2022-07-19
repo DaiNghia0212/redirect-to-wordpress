a3fd329e40b13aceba1a88deb432624d2ec9115c
Vo Huynh Dai Nghia <90742447+DaiNghia0212@users.noreply.github.com>
2022-07-19 21:08:54 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-//   window.location.href = 'https://gearhead1752.000webhostapp.com/';
+  window.location.href = 'https://gearhead1752.000webhostapp.com/';
   return (
     <div className="App">
       <header className="App-header">
