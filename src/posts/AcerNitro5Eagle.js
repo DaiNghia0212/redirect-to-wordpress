61c6659ea86f8bfd21d9628f4e8d63e4eafe6218
jayzernguyen <duynq@qsoftvietnam.com>
2022-07-20 19:47:25 +0700

    add new post

---
+++ src/posts/AcerNitro5Eagle.js
@@ -0,0 +1,7 @@
+function AcerNitro5Eagle() {
+    window.location.href = 'https://gearhead1752.000webhostapp.com/laptop/acer-nitro-5-eagle/';
+    return <h1>Loading...</h1>
+  }
+  
+  export default AcerNitro5Eagle;
+  
