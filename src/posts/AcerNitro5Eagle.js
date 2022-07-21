8102dc7b4278d7a5aaa20e1085c6ce881723c8df
DaiNghia0212 <dainghia12311@gmail.com>
2022-07-21 07:21:02 +0700

    uncomment redirect link

--- src/posts/AcerNitro5Eagle.js
+++ src/posts/AcerNitro5Eagle.js
@@ -2,7 +2,7 @@ import logo from "../logo.svg";
 import '../App.css';
 
 function AcerNitro5Eagle() {
-    // window.location.href = 'https://gearhead1752.000webhostapp.com/laptop/acer-nitro-5-eagle/';
+    window.location.href = 'https://gearhead1752.000webhostapp.com/laptop/acer-nitro-5-eagle/';
     return (
       <div className="App">
         <header className="App-header">
--- src/posts/LaptopGaming.js
+++ src/posts/LaptopGaming.js
@@ -2,7 +2,7 @@ import logo from "../logo.svg";
 import '../App.css';
 
 function LaptopGaming() {
-  // window.location.href = 'https://gearhead1752.000webhostapp.com/laptop/acer-nitro-5';
+  window.location.href = 'https://gearhead1752.000webhostapp.com/laptop/acer-nitro-5';
   return (
     <div className="App">
       <header className="App-header">
