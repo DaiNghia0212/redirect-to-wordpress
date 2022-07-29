2f457e40476d3418c19b5f615d7cac63ba45c667
DaiNghia0212 <dainghia12311@gmail.com>
2022-07-29 23:14:43 +0700

    change logo and background color

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -1,13 +1,17 @@
-import logo from "../logo.svg";
+import logo from "../web-icon-1.png";
 import "../Redirect.css";
+import { useEffect } from "react";
 
 function Redirect({ url }) {
-  window.location.href = url;
+  useEffect(() => {
+    setTimeout(() => (window.location.href = url), 5000);
+  }, [url]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{"Welcome to Gear Head <3"}</p>
+        <p>{"Welcome to GearHead <3"}</p>
       </header>
     </div>
   );
